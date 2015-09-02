e0913623d770ed2f8f39984c29150dfcce753398
MoA <honomoa@gmail.com>
2015-09-02 14:33:01 +0800

    add encode function

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-var decode = require('base64-decode');
 var crypto = require('crypto');
 
 var ANTI_CHEAT_CODE = 'Fe12NAfA3R6z4k0z';
 var SALT = 'af0ik392jrmt0nsfdghy0';
 
-function parse(save) {
+function md5(text) {
+  var md5sum = crypto.createHash('md5');
+  md5sum.update(text, 'ascii');
+  return md5sum.digest('hex');
+}
+
+function decode(save) {
+  var result, clear = '';
   if (save.indexOf(ANTI_CHEAT_CODE) > -1) {
-    var result = save.split(ANTI_CHEAT_CODE);
-    save = '';
+    result = save.split(ANTI_CHEAT_CODE);
     for (var i = 0; i < result[0].length; i += 2) {
-      save += result[0].charAt(i);
+      clear += result[0].charAt(i);
     }
 
-    var md5 = crypto.createHash('md5');
-    md5.update(save + SALT, 'ascii');
-    if (md5.digest('hex') !== result[1]) {
+    if (md5(clear + SALT) !== result[1]) {
       throw new Error('Bad hash');
     }
   }
@@ -22,8 +25,16 @@ function parse(save) {
     throw new Error('Anti-cheat code not found');
   }
 
-  var data = JSON.parse(decode(save));
+  var data = JSON.parse(new Buffer(clear, 'base64').toString('ascii'));
   return data;
 }
+function encode(json) {
+  var b64 = new Buffer(JSON.stringify(json)).toString('base64');
+  var obscure = b64.split('').join('a');
+  var r = md5(b64 + SALT);
 
-module.exports = parse;
+  return obscure+ANTI_CHEAT_CODE+r;
+}
+
+module.exports.decode = decode;
+module.exports.encode = encode;
